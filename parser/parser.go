b3073b5a3826cdacef95e0ab14e5c19a36987482
Keon Madani <keonmadani@frida.local>
2024-04-30 12:53:25 -0400

    added ability to parse identifiers + tests

--- parser/parser.go
+++ parser/parser.go
@@ -7,13 +7,25 @@ import (
 	"monkey/token"
 )
 
-type Parser struct {
-	lex *lexer.Lexer
+const (
+	_ int = iota
+	LOWEST
+	EQUALS		// ==
+	LESSGREATER // > or <
+	SUM			// +
+	PRODUCT		// *
+	PREFIX 		// -X or !X
+	CALL 		// myFunction(X)
+)
 
+type Parser struct {
+	lex 	  *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors 	  []string
 
-	errors []string
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -25,6 +37,9 @@ func New(l *lexer.Lexer) *Parser {
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
 
 	return p
 }
@@ -93,7 +108,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -129,4 +144,49 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+// == Expression Parsing ==
+
+/* 
+Each token can be associated with both a prefix parsing function and an infix parsing function
+depending on the position in which it is found.
+*/
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
