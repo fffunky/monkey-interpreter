7665b031bd28f26f4a99c2cd21c23ad67971acc1
Keon Madani <keonmadani@frida.local>
2024-05-03 16:29:58 -0400

    added functionality to recognize parse prefix operators ('!', '-') [closes #9]

--- ast/ast.go
+++ ast/ast.go
@@ -133,6 +133,29 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+// == Prefix Expression ==
+type PrefixExpression struct {
+	Token 	 token.Token
+	Operator string
+	Right 	 Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 // == Integer Literal ==
 
 type IntegerLiteral struct {
