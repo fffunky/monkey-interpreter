b569a763da1e21c0b37b1db62ef20fdc02782958
Keon Madani <keonmadani@frida.local>
2024-05-21 13:17:25 -0400

    added parsing functionality to REPL [closes #16]

--- repl/repl.go
+++ repl/repl.go
@@ -4,11 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
+
+const MONKEY_ASCII = `
+  ╭~╮
+ C oo⦘
+ _❪ ^❫
+/   ~\
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -21,11 +29,25 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_ASCII)
+	io.WriteString(out, "Whoops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
